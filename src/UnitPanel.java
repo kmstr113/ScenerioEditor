32b4efbe21c04565964a9ec33dd24d0531737ea4
jhahn <jhahn@PC-S1-98.SCHC.com>
2018-09-14 18:58:17 -0400

    Step 2

---
+++ src/UnitPanel.java
@@ -0,0 +1,46 @@
+import javax.swing.*;
+import javax.swing.border.EtchedBorder;
+
+import java.awt.*;
+
+public class UnitPanel extends JPanel {
+	private JTextField uID = null;
+	private JTextField chassisType = null;
+	
+	public UnitPanel() {
+		super();
+		JButton j = new JButton("TEST");	
+		this.setLayout(new BoxLayout(this,BoxLayout.Y_AXIS));
+		
+		uID = new JTextField(10);
+		chassisType = new JTextField(10);
+		
+		JPanel jp = new JPanel();
+		jp.setLayout(new GridLayout(0,2));
+		jp.add(new JLabel("Unit ID"));
+		jp.add(uID);
+		
+		JPanel jp1 = new JPanel();
+		jp1.setLayout(new GridLayout(0,2));
+		jp1.add(new JLabel("Chassis Type"));
+		jp1.add(chassisType);
+		
+		
+		this.add(jp);
+		this.add(jp1);
+		this.add(j);
+		
+		this.setBorder(BorderFactory.createEtchedBorder(EtchedBorder.RAISED ));
+		
+		this.setVisible(true);
+	}
+	
+	public void setUID(String s) {
+		uID.setText(s);
+	}
+	
+	public void setChassisType(String s) {
+		chassisType.setText(s);
+	}
+	
+}
